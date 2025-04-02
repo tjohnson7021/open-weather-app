e2b8746970a46a8100bda0393c8d1f142d927512
Tiffini Johnson <tjohnson@flexion.us>
2025-04-02 16:09:37 -0400

    Successfully connecting to Open Weather API
    - added GET call to weather API

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,3 +1,5 @@
+import axios from "axios";
+import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
 
@@ -5,14 +7,33 @@ dotenv.config();
 
 const app = express();
 
+// Middleware to parse JSON request bodies
+app.use(express.json());
+
+app.use(cors());
+
 // Define routes
 app.get('/', (req, res) => {
-    res.send('Hello, world! <br> you can go to: <br> /status');
+    res.send('Weather API is running... <br> you can go to: <br> /status <br> /weather/[enter zipcode]');
 });
 
 app.get('/status', (req, res) => {
     res.send('Operational!');
 });
+
+app.get("/weather/:zipcode", async (req, res) => {
+    try {
+        const { zipcode } = req.params;
+        const API_KEY = process.env.OPENWEATHER_API_KEY;
+        const response = await axios.get(
+            `https://api.openweathermap.org/data/2.5/forecast?zip=${zipcode}&units=metric&appid=${API_KEY}`
+        );
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).json({ error: "Failed to fetch weather data" });
+    }
+});
+
 
 // Export the app
 export default app;
