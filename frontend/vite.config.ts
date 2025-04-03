d77768cf992445538f07922fee7c3e5c0a3d0623
Tiffini Johnson <tjohnson@flexion.us>
2025-04-03 15:32:23 -0400

    deploy attempt 9
    - updated vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'build', // Make sure this matches your deployment command
+  },
   plugins: [react()],
   base:"/open-weather-app/"
 })
