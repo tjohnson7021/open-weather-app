661e20076dd3fe21fd8307c21e717216e7d351bd
Tiffini Johnson <tjohnson@flexion.us>
2025-04-22 13:34:23 -0400

    Revert "Changed frontend to point at prod"
    This reverts commit 009587c6b0a14b78ec07d088db7d2901d2c97592.

--- frontend/src/components/WeatherForecast.tsx
+++ frontend/src/components/WeatherForecast.tsx
@@ -13,7 +13,7 @@ const WeatherForecast = () => {
     const fetchWeather = async () => {
         try {
             const response = await axios.get(
-                `https://open-weather-app-t8x3.onrender.com/weather/${zip}`
+                `http://localhost:3000/weather/${zip}`
             );
             setWeather(response.data.list);
         } catch (error) {
